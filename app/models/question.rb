61f84d3b933c8c3b2507dadcd94af47497be0209
blobbered <blobbered@gmail.com>
2013-10-02 10:50:43 -0500

    rl - add validation for original text

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ActiveRecord::Base
   validates :display_text, length: { maximum: 500 }
+  validates :original_text, length: { maximum: 500 }
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,16 +3,30 @@ require 'spec_helper'
 describe Question do
 
   before do
-    @question = Question.new(display_text: "question display text here")
+    @question = Question.new(original_text: "question original text here",
+                              display_text: "question display text here")
   end
 
   subject{ @question }
+
+  it { should respond_to(:original_text) }
+  it { should respond_to(:display_text) }
 
   describe "display_text" do
     context "larger than 500 characters" do
       it "throws an error" do
         invalid_text = "0" * 501
         @question.display_text = invalid_text
+        expect(@question).not_to be_valid
+      end
+    end
+  end
+
+  describe "original_text" do
+    context "larger than 500 characters" do
+      it "throws an error" do
+        invalid_text = "0" * 501
+        @question.original_text = invalid_text
         expect(@question).not_to be_valid
       end
     end
