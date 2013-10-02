d711296a92c60222c4b3955910888cb83ea170e0
blobbered <blobbered@gmail.com>
2013-10-02 14:03:28 -0500

    [Ed] Remove original text field from question form

--- spec/views/questions/edit.html.erb_spec.rb
+++ spec/views/questions/edit.html.erb_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe "questions/edit" do
   before(:each) do
     @question = assign(:question, stub_model(Question,
-      :original_text => "MyText",
       :display_text => "MyText"
     ))
   end
@@ -13,7 +12,6 @@ describe "questions/edit" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", question_path(@question), "post" do
-      assert_select "textarea#question_original_text[name=?]", "question[original_text]"
       assert_select "textarea#question_display_text[name=?]", "question[display_text]"
     end
   end
--- spec/views/questions/new.html.erb_spec.rb
+++ spec/views/questions/new.html.erb_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe "questions/new" do
   before(:each) do
     assign(:question, stub_model(Question,
-      :original_text => "MyText",
       :display_text => "MyText"
     ).as_new_record)
   end
@@ -13,7 +12,6 @@ describe "questions/new" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", questions_path, "post" do
-      assert_select "textarea#question_original_text[name=?]", "question[original_text]"
       assert_select "textarea#question_display_text[name=?]", "question[display_text]"
     end
   end
