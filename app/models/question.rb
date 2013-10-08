9a2cf862dff1db438447ac73a151e5ae9a018be2
Ji Wang <jwang@thoughtworks.com>
2013-10-08 16:06:48 -0500

    Ji: #107, show anonymous when the question is submitted anonymously

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,14 +1,20 @@
 class Question < ActiveRecord::Base
+  ANONYMOUS = "Anonymos"
+
   has_many :voting_rounds, through: :voting_round_questions
   has_many :voting_round_questions
 
-  attr_readonly :original_text  
+  attr_readonly :original_text
   before_create :copy_display_text_into_original_text
+  validates :display_text, length: { maximum: 140 , minimum: 1}
+
+  def display_author
+    anonymous ? ANONYMOUS : name
+  end
 
   private
     def copy_display_text_into_original_text
       self.original_text = self.display_text
     end
 
-  validates :display_text, length: { maximum: 500 , minimum: 1}
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,32 +1,47 @@
 require 'spec_helper'
 
 describe Question do
-
-  before do
-    @question = Question.create(display_text: "question display text here")
-  end
-
-  subject{ @question }
+  subject {@question}
 
   context "original_text" do
-    it { should respond_to(:original_text) }
-    it "no updating the original_text" do
-      @question.update_attributes :original_text => 'bar'
-      @question.reload.original_text.should eql 'question display text here'
+    before do
+      @question = FactoryGirl.create(:question)
     end
-    
-    its('original_text') { should eq @question.display_text } 
+
+    it "no updating the original_text" do
+      original_text = @question.original_text
+      @question.update_attributes :original_text => original_text + "new"
+      @question.reload.original_text.should eql original_text
+    end
+
+    its('original_text') { should eq @question.display_text }
   end
 
   context "display_text" do
+    before do
+      @question = FactoryGirl.build(:question)
+    end
+
     it { should respond_to(:display_text) }
     it { should ensure_length_of(:display_text).
-          is_at_least(1).   
-          is_at_most(500) }
+          is_at_least(1).
+          is_at_most(140) }
   end
 
-  it { should respond_to(:name) }
-  it { should respond_to(:neighbourhood) }
-  it { should respond_to(:email) }
-  it { should respond_to(:anonymous) }
+  context "display_author" do
+    context "when the question is submitted anonymously" do
+      it "displays the name" do
+        @question = FactoryGirl.build(:question, :anonymous)
+        @question.display_author.should == Question::ANONYMOUS
+      end
+    end
+
+    context "when the question is not submitted anonymously" do
+      it "displays the name" do
+        @question = FactoryGirl.build(:question)
+        @question.display_author.should == @question.name
+      end
+    end
+  end
+
 end
