e082a19700f7d84afc2b335b406959ac4954c474
Ji Wang <jwang@thoughtworks.com>
2013-10-09 11:11:27 -0500

    Ji: #58, add seed data for categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+
+Category.create([{ name: 'how-we-live', label: 'How we live', active: true},
+                 { name: 'history', label: 'History', active: true},
+                 { name: 'environment', label: 'Environment', active: true},
+                 { name: 'economy', label: 'Economy', active: true},
+                 { name: 'governance', label: 'Governance', active: true},
+                 { name: 'urban-planning', label: 'Urban Planning', active: true},
+                 { name: 'whats-it-like', label: 'What\'s it like to...', active: true}])
