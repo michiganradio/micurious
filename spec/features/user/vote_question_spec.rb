277137ee6b1982921da38a2a53ae1e18771ee244
blobbered <blobbered@gmail.com>
2013-10-09 15:51:01 -0500

    et/rl-108-add question id to vote id

--- spec/features/user/vote_question_spec.rb
+++ spec/features/user/vote_question_spec.rb
@@ -4,15 +4,15 @@ describe "Vote on a question" do
   subject { page }
 
   before { 
-    question = FactoryGirl.create(:question)
+    @question = FactoryGirl.create(:question)
     voting_round = FactoryGirl.create(:voting_round)
-    FactoryGirl.create(:voting_round_question, question_id: question.id,
+    FactoryGirl.create(:voting_round_question, question_id: @question.id,
                                                      voting_round_id: voting_round.id)
     visit root_path
   }
 
   describe "have vote link" do
-    it { should have_link 'Vote' }
+    it { should have_link ('Vote'+@question.id.to_s) }
   end
 
 end
