0bdb9f68749d5494ac63e6077a13315ed11cedc2
Ed Thome <ethome@thoughtworks.com>
2013-10-10 17:11:11 -0500

    [RL/ET] [#144] ]Voting display resets for new voting round

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,7 +9,7 @@ class WelcomeController < ApplicationController
       voting_round_question = VotingRoundQuestion.find_by(question_id: params[:question_id])
       voting_round_question.vote_number += 1
       voting_round_question.save
-      cookies.permanent[:voting_round_id]=voting_round_question.voting_round_id
+      cookies.permanent[:voting_round_id]=VotingRound.last.id
       cookies.permanent[:question_id]=voting_round_question.question_id
       redirect_to root_path
     else
--- spec/features/user/vote_question_spec.rb
+++ spec/features/user/vote_question_spec.rb
@@ -3,18 +3,18 @@ require 'spec_helper'
 describe "Vote on a question" do
   subject { page }
 
-  before(:each) do
-    @question = FactoryGirl.create(:question)
-    @question2 = FactoryGirl.create(:question, :other)
-    voting_round = FactoryGirl.create(:voting_round)
+  before(:each) do                                                                            
+    @question = FactoryGirl.create(:question)                                                 
+    @question2 = FactoryGirl.create(:question, :other)                                        
+    voting_round = FactoryGirl.create(:voting_round)                                          
     voting_round.add_question(@question)
-    voting_round.add_question(@question2)
-    visit root_path
+    voting_round.add_question(@question2)                                                     
+    visit root_path                                                                           
   end
-
+    
   specify "have vote link" do
-    should have_link('vote' + @question.id.to_s)
-  end
+    should have_link('vote' + @question.id.to_s)                                              
+  end                                                                                         
 
   context "after voting" do   
     before { click_link('vote' + @question.id.to_s) }
@@ -30,22 +30,26 @@ describe "Vote on a question" do
     end
 
     specify "voted icon displayed next to voted on question" do
-      should have_selector('img#vote_confirm' + @question.id.to_s)
+      should have_selector('div#vote_confirm' + @question.id.to_s)
     end
 
-    context "new voting round" do
-      before do
-        new_voting_round = FactoryGirl.create(:voting_round, :other)
-        new_voting_round.add_question(@question)
-      end
+  end
 
-      specify "have vote link" do
-        should have_link('vote' + @question.id.to_s)
-      end
+  context "new voting round" do
+    before do
+      click_link('vote' + @question.id.to_s)
+      @new_question = FactoryGirl.create(:question, display_text: "hi")
+      new_voting_round = FactoryGirl.create(:voting_round, :other)
+      new_voting_round.add_question(@new_question)
+      visit(current_path)
+    end
 
-      specify "voted icon not displayed" do
-        should_not have_selector('img#vote_confirm' + @question.id.to_s)
-      end
+    specify "have vote link" do
+      should have_link('vote' + @new_question.id.to_s)
+    end
+
+    specify "voted icon not displayed" do
+      should_not have_selector('div#vote_confirm' + @new_question.id.to_s)
     end
   end
 end
