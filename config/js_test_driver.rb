41471bd83151c06747d67f35eb0f49147fc5b60a
blobbered <blobbered@gmail.com>
2013-10-15 14:06:34 -0500

    rl - 153 - hover on voting round question turns background blue and question image fills height

--- config/js_test_driver.rb
+++ config/js_test_driver.rb
@@ -1,4 +1,4 @@
 # files to be included
 includes 'app/assets/javascripts/*.js'
-includes 'spec/javascript/*.js'
+includes 'spec/javascript/*_spec.js'
 excludes 'app/assets/javascripts/bootstrap.min.js'
