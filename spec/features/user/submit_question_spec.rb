5ed56d9bc3dc3218cd21a99b9c3d64ecb2e40e15
Ed Thome <ethome@thoughtworks.com>
2013-10-17 19:55:09 -0500

    et - 162 - jquery inserts buttons and titles into modal for new and confirm question

--- spec/features/user/submit_question_spec.rb
+++ spec/features/user/submit_question_spec.rb
@@ -1,4 +1,5 @@
 require 'features/features_spec_helper'
+require 'capybara/rails'
 
 require 'selenium-webdriver'
 Capybara.current_driver = :selenium
@@ -13,19 +14,38 @@ describe 'Ask a question', js: true do
     click_button "Ask"
   end
 
-  context "main page" do
-    describe "valid question" do
-      it { should have_content "Submit your question to Curious City" }
-      it { should have_field("question_name") }
-      it { should have_field("question_email") }
-      it { should have_field("question_email_confirmation") }
-      it { should have_field("question_neighbourhood") }
-      it { page.find_by_id("question_display_text").should have_content "Why is the sky blue?" }
-      it { should have_unchecked_field("question_anonymous") }
+  describe "new question modal" do
+    it "has title" do
+      within('#myModal') do
+        should have_content "Submit your question to Curious City"
+      end
+    end
+
+    it "has question fields" do
+      within('#myModal') do
+        should have_field("question_display_text")
+        should have_field("question_name")
+        should have_field("question_email")
+        should have_field("question_email_confirmation")
+        should have_field("question_neighbourhood")
+        should have_unchecked_field("question_anonymous")
+      end
+    end
+
+    it "prepopulates display text field" do
+      within('#myModal') do
+        find_by_id("question_display_text").should have_content "Why is the sky blue?"
+      end
+    end
+
+    it "has next button" do
+      within('#myModal') do
+        should have_selector("#modal-form-next")
+      end
     end
   end
 
-  context "new question page" do
+  describe "confirm question modal" do
     before do
       @category1 = FactoryGirl.create(:category)
       @category2 = FactoryGirl.create(:category, label: "MyString2")
@@ -33,29 +53,43 @@ describe 'Ask a question', js: true do
       find_by_id("display_text").set("Why is the sky blue?")
       click_button "Ask"
 
-      fill_in('question_display_text', with: "Why is the sky green?")
-      fill_in('question_name', with: "Robert Johnson")
-      check('question_anonymous')
+      within('#myModal') do
+        fill_in('question_display_text', with: "Why is the sky green?")
+        fill_in('question_name', with: "Robert Johnson")
+        check('question_anonymous')
 
-      fill_in('question_email', with: "rjohnson@a.com")
-      fill_in('question_email_confirmation', with: "rjohnson@a.com")
-      fill_in('question_neighbourhood', with: "Hell")
+        fill_in('question_email', with: "rjohnson@a.com")
+        fill_in('question_email_confirmation', with: "rjohnson@a.com")
+        fill_in('question_neighbourhood', with: "Hell")
 
-      check('question_category_id_' + @category1.id.to_s)
-      check('question_category_id_' + @category2.id.to_s)
-      click_button "NEXT"
+        check('question_category_id_' + @category1.id.to_s)
+        check('question_category_id_' + @category2.id.to_s)
+        click_link "modal-form-next"
+      end
     end
 
-    specify "confirm details" do
-      should have_content "Double check your question looks good:"
-      should have_content "Why is the sky green?"
-      should have_content "Robert Johnson"
-      should have_content "true"
-      should have_content "rjohnson@a.com"
-      should have_content "Hell"
-      should have_content @category1.label
-      should have_content @category2.label
-      should have_button "SUBMIT"
+    it "has title" do
+      within('#myModal') do
+        should have_content "Double check that your question looks good"
+      end
+    end
+
+    it "displays question details" do
+      within('#myModal') do
+        should have_content "Why is the sky green?"
+        should have_content "Robert Johnson"
+        should have_content "true"
+        should have_content "rjohnson@a.com"
+        should have_content "Hell"
+        should have_content @category1.label
+        should have_content @category2.label
+      end
+    end
+
+    it "has submit button" do
+      within('#myModal') do
+        should have_selector "#modal-form-submit"
+      end
     end
   end
 end
