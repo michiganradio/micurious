4cef389b724a340cf95b72d6aa30945a0fbcafd4
blobbered <blobbered@gmail.com>
2013-10-30 03:12:08 +0800

    rl - #26 - show 50 pictures instead of 3

--- app/services/flickr_service.rb
+++ app/services/flickr_service.rb
@@ -11,7 +11,7 @@ class FlickrService
 
   def find_pictures(search_string)
     Flickrie.api_key = get_api_key
-    query = {:text => search_string, :license=>"1,2,3,4,5,6", :extras=>['owner_name'], :per_page=>3}
+    query = {:text => search_string, :license=>"1,2,3,4,5,6", :extras=>['owner_name'], :per_page=>50}
     Flickrie.search_photos(query)
   end
 end
