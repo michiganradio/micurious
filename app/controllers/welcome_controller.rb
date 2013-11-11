0c5beac3c46ed3dbcca7cdb550bf9490099b5fa3
blobbered <blobbered@gmail.com>
2013-11-11 14:49:55 -0600

    as/rl - #198 - add about page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,13 @@ require 'voting'
 
 class WelcomeController < ApplicationController
   include Voting
-  before_action :load_categories, only: [:home]
+  before_action :load_categories, only: [:home, :about]
 
   def home
     @voting_round = VotingRound.where(status: VotingRound::Status::Live).first
+  end
+
+  def about
   end
 
   def vote
