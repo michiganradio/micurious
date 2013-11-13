7d7b52b46ebd05ee8ffedbd42ecfd3964a4bb593
albertsaave <albert.saave@gmail.com>
2013-11-13 10:14:20 -0600

    AS/RL fix icons and add asset pipeline compilation to dev and qa

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,5 +25,21 @@ Curiouscity::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
+  # Disable Rails's static asset server (Apache or nginx will already do this).
+  # config.serve_static_assets = false
+
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
+
+  # Generate digests for assets URLs.
+  config.assets.digest = true
+
+  # Version of your assets, change this if you want to expire all your assets.
+  config.assets.version = '1.0'
+
   config.assets.debug = true
 end
