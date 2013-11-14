8eb639cf19e318fdac459b12ee44055b9a9ab55a
blobbered <blobbered@gmail.com>
2013-11-14 17:00:47 -0600

    rl - make descriptions of admin voting round spec more documentation-like

--- spec/features/admin/voting_round_spec.rb
+++ spec/features/admin/voting_round_spec.rb
@@ -1,6 +1,6 @@
 require 'features/features_spec_helper'
 
-describe "Manage voting round" do
+describe "admin's voting round actions" do
 
   before { signin_as_admin }
 
@@ -11,7 +11,7 @@ describe "Manage voting round" do
       @label_text = "label text"
     end
 
-    context "label field filled in" do
+    context "when label field is not empty" do
       before do
         @new_admin_voting_round_page.label.set @label_text
         @new_admin_voting_round_page.create_button.click
@@ -30,19 +30,19 @@ describe "Manage voting round" do
       end
     end
 
-    context "label field not filled in" do
+    context "when label field is empty" do
       before do
         @new_admin_voting_round_page.create_button.click
         @default_label_text =  "Voting Round " + VotingRound.last.id.to_s
       end
 
-      it "displays label on index page" do
+      it "displays default label text on index page as the label" do
         @show_admin_voting_round_page = Admin::ShowVotingRound.new
         @show_admin_voting_round_page.load(id: VotingRound.last.id)
         @show_admin_voting_round_page.label.text.should == @default_label_text
       end
 
-      it "displays label on voting round show page" do
+      it "displays default label text on voting round show page" do
         @admin_voting_rounds_page = Admin::VotingRounds.new
         @admin_voting_rounds_page.load
         expect(@admin_voting_rounds_page.has_label? /#{@default_label_text}/).to be_true
@@ -50,20 +50,21 @@ describe "Manage voting round" do
     end
   end
 
-  describe "edit voting round label" do
-    before do
-      @voting_round = FactoryGirl.create(:voting_round)
-      @edit_admin_voting_round_page = Admin::EditVotingRound.new
-      @edit_admin_voting_round_page.load(id: @voting_round.id)
-      @edit_admin_voting_round_page.label.set "new label text"
-      @edit_admin_voting_round_page.update_button.click
-    end
+  describe "edit voting round" do
+    context "when label is changed" do
+      before do
+        @voting_round = FactoryGirl.create(:voting_round)
+        @edit_admin_voting_round_page = Admin::EditVotingRound.new
+        @edit_admin_voting_round_page.load(id: @voting_round.id)
+        @edit_admin_voting_round_page.label.set "new label text"
+        @edit_admin_voting_round_page.update_button.click
+      end
 
-    it "displays new label text on voting round show page" do
-      @show_admin_voting_round_page = Admin::ShowVotingRound.new
-      @show_admin_voting_round_page.load(id: @voting_round.id)
-      @show_admin_voting_round_page.label.text.should ==
-          "new label text"
+      it "displays updated label text on voting round show page" do
+        @show_admin_voting_round_page = Admin::ShowVotingRound.new
+        @show_admin_voting_round_page.load(id: @voting_round.id)
+        @show_admin_voting_round_page.label.text.should == "new label text"
+      end
     end
   end
 end
