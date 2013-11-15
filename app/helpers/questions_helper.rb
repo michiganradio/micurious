faa8baa75aea249dbf297d40149c7e1bad574011
Ji Wang <jwang@thoughtworks.com>
2013-11-15 16:13:43 -0600

    JW: add Russian Dole caching for user question index page

---
+++ app/helpers/questions_helper.rb
@@ -0,0 +1,7 @@
+module QuestionsHelper
+  def cache_key_for_questions(questions)
+    max_updated_at = questions.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "questions-#{params[:status]}-#{params[:category_name]}-#{questions.count}-#{max_updated_at}"
+  end
+end
+
