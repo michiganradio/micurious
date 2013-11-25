7e7dac0a5ba3d6b039380b986b0b51f136c3e11a
Ed Thome <ethome@thoughtworks.com>
2013-11-25 10:05:42 -0600

    JW/ET - Fix widget loading in iframe

--- app/controllers/widget_controller.rb
+++ app/controllers/widget_controller.rb
@@ -3,6 +3,8 @@ require 'voting'
 class WidgetController < ApplicationController
   include Voting
 
+  before_action :set_response_header
+
   def vote_widget
     @voting_round = VotingRound.where(status: VotingRound::Status::Live).first
     render layout: "widget"
@@ -18,5 +20,11 @@ class WidgetController < ApplicationController
 
   def ask_widget
     render layout: "widget"
+  end
+
+  private
+
+  def set_response_header
+    response.headers["X-Frame-Options"] = "ALLOWALL"
   end
 end
