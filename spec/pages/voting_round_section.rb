ca15ba82c43fa4490aa7d80cf015a6921216bc2b
Ed Thome <ethome@thoughtworks.com>
2013-12-04 17:53:14 -0600

    ET #218 - mobile style refactoring

--- spec/pages/voting_round_section.rb
+++ spec/pages/voting_round_section.rb
@@ -1,8 +1,8 @@
 class VotingRoundSection < SitePrism::Section
-  elements :vote_links, ".vote"
+  elements :vote_links, ".vote:not(.visible-xs)"
   elements :question_links, "div.question-text > a.question-link"
   elements :picture_links, "span.image-credits > a"
   elements :pictures, "div.question-image img"
   elements :ranks, "h2.rank"
-  element :vote_confirm, "div.voted"
+  element :vote_confirm, "div.voted:not(.visible-xs)"
 end
