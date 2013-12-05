47533365c47fb4a452145e1bd0cc1c60b08a3fc2
blobbered <blobbered@gmail.com>
2013-12-05 12:54:42 -0600

    rl - gpl license added.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+=begin
+Copyright 2013 WBEZ
+This file is part of Curious City.
+
+Curious City is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Curious City is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Curious City.  If not, see <http://www.gnu.org/licenses/>.
+=end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
